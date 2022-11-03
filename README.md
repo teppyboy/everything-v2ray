4194cc71b3a84906b1dfdba877d9c93881c9f5a8
tretrauit <tretrauit@gmail.com>
2022-11-03 22:18:59 +0700

    repo: add ui

--- README.md
+++ README.md
@@ -7,5 +7,8 @@ There are currently 2 app configs
 + [Matsuri](config/matsuri.json)
 + [SagerNet](config/sagernet.json)
 
-# Certificate
+## Certificate
 They are generated dummy certificate, replace with your own if you wish to pass the TLS check.
+
+## Web UI
+There are yacd and clash-dashboard available by default
--- scripts/manager.py
+++ scripts/manager.py
@@ -1,2 +1,18 @@
-import subprocess
+from subprocess import run, PIPE
+import argparse
 
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="manager",
+        description="Manage the application",
+        epilog="#KurumiTokisaki",
+    )
+    subparsers = parser.add_subparsers(required=True)
+    parser_a = subparsers.add_parser("vpn", help="sing-box related commands")
+    parser_a = subparsers.add_parser("vpn-config", help="sing-box related commands")
+    parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
